4b87010f659673ed2e1a048166dbba3744b5dccb
rankdjr <douglas.rankjr@gmail.com>
2024-04-12 23:20:37 -0700

    added models

---
+++ HabitTrackerConsole/Models/LogEntry.cs
@@ -0,0 +1,10 @@
+﻿namespace HabitTrackerConsole.Models
+{
+    internal class LogEntry
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int HabitId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ HabitTrackerConsole/Models/LogEntryViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace HabitTrackerConsole.Models
+{
+    internal class LogEntryViewModel
+    {
+        public int RecordId { get; set; }
+        public DateTime Date { get; set; }
+        public string? HabitName { get; set; }
+        public int Quantity { get; set; }
+    }
+}
